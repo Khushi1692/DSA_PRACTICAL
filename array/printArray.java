02ee1b8c4c33817f4472a234fc09021cf04c20ea
Khushi <87716798+Khushi1692@users.noreply.github.com>
2021-12-21 23:08:35 +0530

    Day-2

---
+++ array/printArray.java
@@ -0,0 +1,10 @@
+class Solution {
+    //Just print the space seperated array elements
+    void printArray(int arr[], int n) {
+        int i;
+        for(i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
