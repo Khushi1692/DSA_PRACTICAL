0feb027f0536d837a6a89e91d83b0d1904e37a09
Khushi <87716798+Khushi1692@users.noreply.github.com>
2021-12-22 22:57:12 +0530

    Day-3

---
+++ array/playing_with_or.java
@@ -0,0 +1,16 @@
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] game_with_number (int arr[], int n) {
+        // Complete the function
+        int i;
+        for(i=0;i<n-1;i++)
+        {
+            arr[i]=arr[i]^arr[i+1];
+        }
+        return arr;
+    }
+    
+    
+}
