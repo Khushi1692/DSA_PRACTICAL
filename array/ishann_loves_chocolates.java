a748dd827b6aa50f897ad8a5d182bcc955e35ea8
Khushi <87716798+Khushi1692@users.noreply.github.com>
2021-12-23 22:37:51 +0530

    Day-4

---
+++ array/ishann_loves_chocolates.java
@@ -0,0 +1,19 @@
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int chocolates (int arr[], int n) {
+        //Complete the function
+        int min=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+    
+    
+}
